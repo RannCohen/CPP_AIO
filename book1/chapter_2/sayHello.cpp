c5fc7b80a695d65456b724987a3188cc7dcebce0
Ran Cohen <ranchn1377@gmail.com>
2022-03-27 21:22:03 +0300

    init - first three chapters

---
+++ book1/chapter_2/sayHello.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+    cout << "Hello, Ran" << endl;
+    cout << 5 + 10 << endl;
+
+    return 0;
+}
---
+++ book1/chapter_3/createString.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string myString;
+    myString = "abcdef";
+    cout << myString[2] << endl;
+    return 0;
+}
