647bdef7e410ff0f637c43b894b63f881e27963f
Ran Cohen <ranchn1377@gmail.com>
2022-03-28 15:20:30 +0300

    got to page 100 in less then a week!

--- book1/chapter_3/createString.cpp
+++ book1/chapter_3/createString.cpp
@@ -7,5 +7,10 @@ int main()
     string myString;
     myString = "abcdef";
     cout << myString[2] << endl;
+    char myChar;
+    myChar = myString[2];
+    cout << myChar << endl;
+    myString += " ghijk";
+    cout << myString << endl;
     return 0;
 }
